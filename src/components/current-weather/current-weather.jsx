0e3d97df900868b4326190140399ff5db50edd86
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-30 23:14:58 +0300

    css done

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -1,30 +1,42 @@
 import "./current-weather.css";
 
 const CurrentWeather = () => {
-    return (
-        <>
-        <div className="weather">
-            <div className="top">
-                <div>
-                <p className="city">Kaunas</p>
-                <p className="weather-desc">Sunny</p>
-                </div>
-            <img src="icons/01d.png" className="weather-icon" alt="weather" />
-            </div>
-            <div className="bottom">
-                <p className="temperature">18C</p>
-                <div className="details">
-                    <div className="parameter-row">
-                        <span className="parameter-label">Details</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Feels like</span>
-                        <span className="parameter-value">22C</span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="weather">
+        <div className="top">
+          <div>
+            <p className="city">Kaunas</p>
+            <p className="weather-desc">Sunny</p>
+          </div>
+          <img src="icons/01d.png" className="weather-icon" alt="weather" />
         </div>
-        </>
-    )
-}
-export default CurrentWeather
+        <div className="bottom">
+          <p className="temperature">18C</p>
+          <div className="details">
+            <div className="parameter-row">
+              <span className="parameter-label top">Details</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Feels like</span>
+              <span className="parameter-value">22C</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">wind</span>
+              <span className="parameter-value">2m/s</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Humidity</span>
+              <span className="parameter-value">15%</span>
+            </div>
+            <div className="parameter-row">
+              <span className="parameter-label">Pressure</span>
+              <span className="parameter-value">15 hPa</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default CurrentWeather;
