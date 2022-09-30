c6c961d4b4d898157b4166757d82145fc439485f
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-30 12:42:49 +0300

    search component

---
+++ src/components/search/search.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { METEO_API_URL, meteoApiOptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(`${METEO_API_URL}/places&namePrefix=${inputValue}`, options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Įveskite miestą... pvz. Vilnius"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
